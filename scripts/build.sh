0d100b0d4e97e7db7d77613081303307c48cba7d
Luna D <luna@nighty.cloud>
2023-07-09 12:56:30 +0200

    fix aws

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export $(cat /home/app/docker/.env | xargs)
+
 docker build --build-arg UID=$(id -u) --network host -t app ./app
 docker build --build-arg UID=$(id -u) --network host -t db ./app/docker-resources/database
-docker build --network host -t web ./web
+docker build --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --network host -t web ./web

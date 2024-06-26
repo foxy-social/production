0e7453dd8d27aaf1df3f2a2628610ba729bfda2b
Luna D. <luna@nighty.cloud>
2024-06-26 19:38:08 +0200

    http2

--- services/start-app.sh
+++ services/start-app.sh
@@ -7,7 +7,7 @@ docker create \
   --hostname foxysocial \
   --env-file /home/foxy/docker/.env \
   -v /home/foxy/docker/app:/opt/akkoma \
-  -p 10.100.0.2:5000:4000 \
+  -p 10.100.0.1:5000:4000 \
   foxyapp:latest
 
 docker network connect foxy-network-web foxy-app
